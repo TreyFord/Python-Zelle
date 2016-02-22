b21c8eeef5b34fb96fd947b00834bc80aeaaea77
TreyFord <trey@desonnaille.com>
2016-02-22 08:48:23 -0600

    initial chaos project

--- chaos-advanced.py
+++ chaos-advanced.py
@@ -2,8 +2,9 @@
 # A simple program illustrating chaotic behavior
 
 print("This program illustrates a chaotic function")
-x = eval(input("Enter a number between 0 and 1:"))
-for i in range(20):
+x = eval(input("Enter a number between 0 and 1: "))
+y = eval(input("Enter how many numbers should I print: "))
+for i in range(y):
     x = 3.9 * x * (1 - x)
     print(x)
 
