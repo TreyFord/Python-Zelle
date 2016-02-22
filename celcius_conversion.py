139492b80aeaed937ed07eae249e743d61a0573e
TreyFord <trey@desonnaille.com>
2016-02-22 09:12:26 -0600

    initial chaos project

---
+++ celcius_conversion.py
@@ -0,0 +1,7 @@
+#File: celcius_conversion .py
+# A simple program to convert celcius to fahrenheit temperatures.
+
+print("A quick and simple conversion program for temperatures")
+c = eval(input("What is the temperature in Celcius?"))
+f = ((9/5)*c+32)
+print("The temperature in Farenheit is", f)
