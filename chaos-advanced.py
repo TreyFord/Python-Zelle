04239407b72641eae2d6995bb4f1bbea2855aaac
TreyFord <trey@desonnaille.com>
2016-02-22 08:56:06 -0600

    initial chaos project

--- chaos-advanced.py
+++ chaos-advanced.py
@@ -3,9 +3,11 @@
 
 print("This program illustrates a chaotic function")
 x = eval(input("Enter a number between 0 and 1: "))
-y = eval(input("Enter how many numbers should I print: "))
-for i in range(y):
+y = eval(input("Enter a number to compare between 0 and 1: "))
+z = eval(input("Enter how many numbers should I print: "))
+for i in range(z):
     x = 3.9 * x * (1 - x)
-    print(x)
+    y = 3.9 * y * (1 - y)
+    print(x, "     ",y)
 
 # main()
